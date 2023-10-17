7f1af2dbb0aa42808134d6d09e3e796998302195
AmV1to <amvito@yandex.ru>
2023-10-17 22:23:06 +0500

    refactoring card cloud data source

--- app/src/main/java/com/github/amvito/cards/cards/data/cloud/CardService.kt
+++ app/src/main/java/com/github/amvito/cards/cards/data/cloud/CardService.kt
@@ -1,10 +1,11 @@
 package com.github.amvito.cards.cards.data.cloud
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CardService {
 
     @GET("credit_cards")
-    suspend fun getCards(@Query("_quantity")countCards:Int): ResponseFakerApi
+    suspend fun getCards(@Query("_quantity")countCards:Int): Response<ResponseFakerApi>
 }
